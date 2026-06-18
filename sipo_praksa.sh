706a0950accf6302d8c749898285c9347abb20fd
MajcenClarkeJ <jasmine.majcenc@gmail.com>
2026-06-18 12:50:00 +0200

    Update sipo_praksa.sh

--- sipo_praksa.sh
+++ sipo_praksa.sh
@@ -1,8 +1,29 @@
-echo Kaj je OS? - je programska oprema, ki omogoča delovanje računalnika. Deluje kot posrednik med strojno opremo in uporabnikom, kar omogoča interakcijo in upravljanje programov.
-echo 3 naloge - zaščita podatkov in procesov, upravljanje z datotekami, podpora programskim jezikom.
-echo linux/windows/mac OS - Linux in MAC so priljubljeni izbi, saj obiskovalci najbolj uporabljajo Windows. Linux je najmanj uporabljen operacijski sistem z uporabniki, ki predstavljajo 1%, medtem ko MAC je priljubljen in ima celotno uporabniško bazo 7% po vsem svetu. Windows je najbolj nagnjen za tveganje zlonamerno programske opreme, kar je posledica večje baze uporabnikov. Linux verjetno ne bo vplivala zlonamerna programska oprema.
-echo kaj pomeni da se "naloži ob zagonu"?- to pomeni, da je program, ki ga naloži, potrebno za naložbo operacijskega sistema ali izvajalnega okolja. To je ključno za pravilno in učinkovito delovanje računalnika. Programi, ki se naložijo ob zagonu, so lahko različni, vključno z gonilniki, sistemskimi aplikacijami in programsko opremo. 
-echo kaj je gonilnik? - je posebna programska oprema, ki operacijskemu sistemu omogoča komunikacijo s strojno opremo
-echo kje ga potrebujemo - Gonilnike potrebujemo vedno, ko želimo uporabljati zunanje ali notranje komponente računalnika. 
-echo HDD vs SSD - SSD-ji (Solid State Drives) so bistveno hitrejši, vzdržljivejši in tišji od HDD-jev (HDD-jev)
-echo kaj je particija? - ) je logična razdelitev fizičnega trdega diska (HDD ali SSD) na več neodvisnih delov, ki jih operacijski sistem obravnava kot samostojne pogone
+Kaj je OS?
+- je programska oprema, ki omogoča delovanje računalnika. Deluje kot posrednik med strojno 
+  opremo in uporabnikom, kar omogoča interakcijo in upravljanje programov.
+  
+3 naloge 
+- zaščita podatkov in procesov, upravljanje z datotekami, podpora programskim jezikom.
+
+linux/windows/mac OS 
+- Linux in MAC so priljubljeni izbi, saj obiskovalci najbolj uporabljajo Windows. Linux je najmanj uporabljen
+  operacijski sistem z uporabniki, ki predstavljajo 1%, medtem ko MAC je priljubljen in ima celotno uporabniško
+  bazo 7% po vsem svetu. Windows je najbolj nagnjen za tveganje zlonamerno programske opreme, kar je posledica 
+  večje baze uporabnikov. Linux verjetno ne bo vplivala zlonamerna programska oprema.
+
+kaj pomeni da se "naloži ob zagonu"?
+- to pomeni, da je program, ki ga naloži, potrebno za naložbo operacijskega sistema ali izvajalnega okolja. 
+To je ključno za pravilno in učinkovito delovanje računalnika. Programi, ki se naložijo ob zagonu, so lahko 
+različni, vključno z gonilniki, sistemskimi aplikacijami in programsko opremo.
+
+kaj je gonilnik?
+- je posebna programska oprema, ki operacijskemu sistemu omogoča komunikacijo s strojno opremo.
+
+kje ga potrebujemo 
+- Gonilnike potrebujemo vedno, ko želimo uporabljati zunanje ali notranje komponente računalnika. 
+
+HDD vs SSD 
+- SSD-ji (Solid State Drives) so bistveno hitrejši, vzdržljivejši in tišji od HDD-jev (HDD-jev).
+
+kaj je particija? - ) je logična razdelitev fizičnega trdega diska (HDD ali SSD) na več neodvisnih
+delov, ki jih operacijski sistem obravnava kot samostojne pogone
